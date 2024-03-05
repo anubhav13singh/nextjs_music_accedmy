840fde529052f975b844f149d6a3f950449378eb
Anubhav Singh <singhanubhav81717@gmail.com>
2024-03-05 13:00:15 +0530

    featured courses

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 ">
+    <div className="h-auto md:h-[40rem] w-full rounded flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="pink" />
       <div className="p-4 relative z-10 text-center w-full">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 ">
